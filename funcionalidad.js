362d98d500cbad82d71dda64feb2ebf64517f586
Jose Martos Martin <martosmartinjose@gmail.com>
2017-02-22 13:24:49 +0100

    Añadida funcionalidad JavaScript a division y calculadora

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,41 +1,64 @@
 
-function limpiar(){
-	$("#a").text("");
-	$("#b").text("");
-	$("#op").text("");
-	$("#resul").text("");
-	operando="";
-	a=null;
-	b=null;
-	c=2;
-	$("#error").text("");
-}
-
-
 $(function(){
 	var operando="";
 	var a=null;
 	var b=null;
 	var c=2;
-
+	var resultado;
+	
 	$("#igual").click(function(){
+		console.log(a+" "+b);
 		if (a!=null && b!=null){
-			if (operando=="+"){
+			var soGood=false;
+			switch (operando){
+			case "+":
 				resultado=parseInt(a)+parseInt(b);
 				console.log("suma");
-			}else{
+				soGood=true;
+				break;
+			case "-":
 				console.log("resta");
 				resultado=(parseInt(a))-(parseInt(b));
+				soGood=true;
+				break;
+			case "*":
+				console.log("multiplicacion");
+				resultado=(parseInt(a))*(parseInt(b));
+				soGood=true;
+				break;
+			case "/":
+				
+				console.log(parseInt(b)==0);
+				if (parseInt(b)===0){
+					console.log("Division por 0");
+					$("#error").text("No puedes dividir por 0!");
+				}else{
+					console.log("division");
+					soGood=true;
+					resultado=(parseInt(a))/(parseInt(b));
+				}
+				break;
 			}
-			console.log(resultado);
-			$("#error").text("");
-			$("#a").text(resultado);
-			$("#b").text("");
-			$("#op").text("");
-			b=null;
-			c=2;
-			operando="";
-			a=resultado;
+			if (soGood){
+				console.log(resultado);
+				$("#error").text("");
+				$("#a").text(resultado);
+				$("#b").text("");
+				$("#op").text("");
+				b=null;
+				c=2;
+				operando="";
+				a=resultado;
+				resultado=null;
+			}else{
+				$("#a").text("");
+				$("#b").text("");
+				$("#op").text("");
+				operando="";
+				a=null;
+				b=null;
+				c=2;
+			}
 			
 		}else{
 			$("#error").text("Debes definir 2 valores para realizar una operacion");
@@ -67,7 +90,7 @@ $(function(){
 	$("button").click(function(){
 		var boton=this.value;
 		
-		if(boton!="-" && boton!="+" && boton!="" && boton!="igual"){
+		if(boton!="-" && boton!="+" && boton!="" && boton!="igual" && boton!="*" && boton!="/"){
 			console.log(boton);
 			if(c%2==0){
 				boton=$("#a").text()+boton;
@@ -82,4 +105,15 @@ $(function(){
 		}
 	});
 });
+
+function limpiar(){
+	$("#a").text("");
+	$("#b").text("");
+	$("#op").text("");
+	operando="";
+	a=null;
+	b=null;
+	c=2;
+	$("#error").text("");
+}
 
