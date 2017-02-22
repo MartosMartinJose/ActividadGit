7d6e1c380515d9646b37f1e4155bc6c09bd73d21
Jose Martos Martin <martosmartinjose@gmail.com>
2017-02-22 12:36:23 +0100

    Actualizado index y funcionalidad con sumas y restas y botones a tamaño completo responsivos

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,71 +1,3 @@
-var operando="";
-var a=null;
-var b=null;
-var c=2;
-
-$("#igual").click(function(){
-	if (a!=null && b!=null){
-		if (operando=="+"){
-			resultado=parseInt(a)+parseInt(b);
-			console.log("suma");
-		}else{
-			console.log("resta");
-			resultado=(parseInt(a))-(parseInt(b));
-		}
-		console.log(resultado);
-		$("#error").text("");
-		$("#a").text(resultado);
-        $("#b").text("");
-        $("#op").text("");
-        b=null;
-        c=2;
-        operando="";
-        a=resultado;
-        
-	}else{
-		$("#error").text("Debes definir 2 valores para realizar una operacion");
-	}
-});
-
-$(".opera").click(function(){
-	console.log("opera");
-	if (operando=="" && a!=null){
-		console.log("ope1");
-		operando=this.value;
-		$("#op").text(operando);
-		c++;
-	}else{
-		if (a==null){
-			$("#error").text("Introduce algun numero primero");
-		}else{
-			if (operando!=""){
-				$("#error").text("Ya has seleccionado un operador");	
-			}else{
-				$("#error").text("Error de operación");
-			}
-		}	
-	}
-});
-
-$("#limp").click(limpiar);
-
-$("button").click(function(){
-	var boton=this.value;
-	
-	if(boton!="-" && boton!="+" && boton!="" && boton!="igual"){
-		console.log(boton);
-		if(c%2==0){
-			boton=$("#a").text()+boton;
-			$("#a").text(boton);
-			a=boton;
-		}else{
-			boton=$("#b").text()+boton;
-			$("#b").text(boton);	
-			b=boton;
-		}
-		
-	}
-});
 
 function limpiar(){
 	$("#a").text("");
@@ -78,3 +10,76 @@ function limpiar(){
 	c=2;
 	$("#error").text("");
 }
+
+
+$(function(){
+	var operando="";
+	var a=null;
+	var b=null;
+	var c=2;
+
+	$("#igual").click(function(){
+		if (a!=null && b!=null){
+			if (operando=="+"){
+				resultado=parseInt(a)+parseInt(b);
+				console.log("suma");
+			}else{
+				console.log("resta");
+				resultado=(parseInt(a))-(parseInt(b));
+			}
+			console.log(resultado);
+			$("#error").text("");
+			$("#a").text(resultado);
+			$("#b").text("");
+			$("#op").text("");
+			b=null;
+			c=2;
+			operando="";
+			a=resultado;
+			
+		}else{
+			$("#error").text("Debes definir 2 valores para realizar una operacion");
+		}
+	});
+
+	$(".opera").click(function(){
+		console.log("opera");
+		if (operando=="" && a!=null){
+			console.log("ope1");
+			operando=this.value;
+			$("#op").text(operando);
+			c++;
+		}else{
+			if (a==null){
+				$("#error").text("Introduce algun numero primero");
+			}else{
+				if (operando!=""){
+					$("#error").text("Ya has seleccionado un operador");	
+				}else{
+					$("#error").text("Error de operación");
+				}
+			}	
+		}
+	});
+
+	$("#limp").click(limpiar);
+
+	$("button").click(function(){
+		var boton=this.value;
+		
+		if(boton!="-" && boton!="+" && boton!="" && boton!="igual"){
+			console.log(boton);
+			if(c%2==0){
+				boton=$("#a").text()+boton;
+				$("#a").text(boton);
+				a=boton;
+			}else{
+				boton=$("#b").text()+boton;
+				$("#b").text(boton);	
+				b=boton;
+			}
+			
+		}
+	});
+});
+
